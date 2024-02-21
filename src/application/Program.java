e54d9a1999d2b66e98efb175c685f0c884aafa92
Guilherme Suganame <gsuganame@gmail.com>
2024-02-21 17:46:30 -0300

    feat: insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,8 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -9,6 +11,9 @@ import model.entities.Seller;
 
 public class Program {
 	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+
 		System.out.println("=== TESTE 1: Seller findById ===");
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 
@@ -30,5 +35,14 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+
+		System.out.println("\n=== TESTE 4: Seller insert===");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00, department);
+
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = " + newSeller.getId());
+//		for (Seller obj : list) {
+//			System.out.println(obj);
+//		}
 	}
 }
